7d650d864216251ca92755e0d384909dc950dffd
Aless <titifavotto@gmail.com>
2024-10-09 18:52:27 +0000

    juegoparajavier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
+  let attempts = 0;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  window.guessNumberGame = function() {
+    const userGuess = document.getElementById("guessInput").value;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    if (isNaN(userGuess)) {
+      alert("Ingresa un numero valido.");
+      return;
+    }
+
+    const guess = parseInt(userGuess);
+    attempts++;
+
+    if (guess === randomNumber) {
+      alert(
+        `Felicitaciones Javier! Adivinaste el numero correcto en ${attempts} intentos.`
+      );
+
+      randomNumber = Math.floor(Math.random() * 10) + 1;
+      attempts = 0;
+      document.getElementById("guessInput").value = "";
+    } else if (guess > randomNumber) {
+      alert("Tu numero es muy alto Javier, intenta uno mas bajo.");
+    } else {
+      alert("Tu numero es muy bajo Javier, intenta uno mas alto.");
+    }
+  };
+});
